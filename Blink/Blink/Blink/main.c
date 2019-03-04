58b0d59a53f79c5adb70299abe1c500c3326dc4c
Denis von Arx <dvarx@gmx.ch>
2019-03-04 20:42:12 +0100

    initial commit to the AVR repository

---
+++ Blink/Blink/Blink/main.c
@@ -0,0 +1,23 @@
+//use this preprocessor macro to define the clockspeed of ATMEGA
+//the clock speed can be adjusted by setting the LOW Fuse Byte (0xFF)
+//and using a 16MHz crystal with two 22pF cermaic capacitors
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	//DDRB=Data Direction Register, 0=input, 1=output
+	DDRB=0b00000001;
+	while (1)
+	{
+		//PORTB defines the current output of Port B
+		PORTB=0b00000001;
+		_delay_ms(100);
+		PORTB=0b00000000;
+		_delay_ms(100);
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Readme:
+
+1.)	In order to program the ATMEGA328P from ATMEL Studio, connect the ISP as described in
+	atmega_setup.png. Fig 1 shows the pinout of the ATMEL ICE connector.
